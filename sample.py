7d6ab91e0d1d825eb045255bf036aa6166074182
dicreltd <skohara@dicre.com>
2020-04-18 22:04:54 +0900

    change2

--- sample.py
+++ sample.py
@@ -3,9 +3,9 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')   
 
 layout = [  [sg.Text('足し算')],
-            [sg.Text('数1'), sg.InputText()],
-            [sg.Text('数2'), sg.InputText()],
-            [sg.Text('',key='ans'), ],
+            [sg.Text('数１'), sg.InputText()],
+            [sg.Text('数２'), sg.InputText()],
+            [sg.Text('0',key='ans'), ],
             [sg.Button('計算'), sg.Button('Cancel')] ]
 
 window = sg.Window('Window Title', layout)
